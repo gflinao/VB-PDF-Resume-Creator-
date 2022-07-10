e7adde21484b1a27e6cc1d42ebd2295e16bb9571
Genrev Linao <gflinao@iskolarngbayan.pup.edu.ph>
2022-07-10 19:18:04 +0800

    Added button for Generate / Name changes for Form and button

---
+++ PDF Resume Creator VB/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ResCre
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnGen = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnGen
+        '
+        Me.BtnGen.Location = New System.Drawing.Point(86, 88)
+        Me.BtnGen.Name = "BtnGen"
+        Me.BtnGen.Size = New System.Drawing.Size(111, 67)
+        Me.BtnGen.TabIndex = 0
+        Me.BtnGen.Text = "Generate"
+        Me.BtnGen.UseVisualStyleBackColor = True
+        '
+        'ResCre
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 258)
+        Me.Controls.Add(Me.BtnGen)
+        Me.Name = "ResCre"
+        Me.Text = "PDF Resume Creator"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnGen As Button
+End Class
---
+++ PDF Resume Creator VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PDF Resume Creator VB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PDF Resume Creator VB")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("60499e49-c969-4fa4-bc4c-ed4c3021b1d8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
